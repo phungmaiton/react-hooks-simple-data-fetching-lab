f92c68425c6cb1ccc3555753ebc5e55a29cd6b5d
Mai <garcia.ton94@gmail.com>
2023-05-29 15:06:22 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import { useState, useEffect } from "react";
+
+function App() {
+  const [image, setImage] = useState("");
+  const [isLoaded, setIsLoad] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((resp) => resp.json())
+      .then((image) => {
+        setImage(image.message);
+        setIsLoad(true);
+      });
+  }, []);
+
+  if (!isLoaded) return <p>Loading...</p>;
+  return (
+    <div>
+      <img alt="A Random Dog" src={image} />
+    </div>
+  );
+}
+
+export default App;
